51c935dc8ae4e33d61430bd0aaa1b72ea89f3792
jakobehmsen <jakobehmsen.github@gmail.com>
2015-03-02 12:21:04 +0100

    Added test for negating boolean expressions.

---
+++ intellij/astava/src/test/java/astava/java/gen/NotTest.java
@@ -0,0 +1,37 @@
+package astava.java.gen;
+
+import astava.CommonTest;
+import astava.java.Descriptor;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+import java.util.Collection;
+
+import static astava.java.Factory.literal;
+import static astava.java.Factory.not;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class NotTest {
+    private boolean value;
+
+    public NotTest(boolean value) {
+        this.value = value;
+    }
+
+    @Parameterized.Parameters
+    public static Collection values() {
+        return Arrays.asList(new Object[]{true}, new Object[]{false});
+    }
+
+    @Test
+    public void testNot() throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+        boolean expectedValue = !value;
+
+        CommonTest.testExpression(not(literal(value)), Descriptor.BOOLEAN, (Boolean actualValue) ->
+            assertEquals(expectedValue, actualValue));
+    }
+}
