f8fffcb8f1e1f978df8b0f8c3e3210f2ec5ea42b
jakobehmsen <jakobehmsen.github@gmail.com>
2015-03-04 20:24:48 +0100

    Added code generation support for ternary expressions.

--- intellij/astava/src/main/java/astava/java/gen/ClassGenerator.java
+++ intellij/astava/src/main/java/astava/java/gen/ClassGenerator.java
@@ -427,6 +427,21 @@ public class ClassGenerator {
                 generator.instanceOf(t);
 
                 return Descriptor.BOOLEAN;
+            } case ASTType.TERNARY: {
+                Tuple condition = expression.getTupleProperty(Property.KEY_CONDITION);
+                Tuple ifTrue = expression.getTupleProperty(Property.KEY_IF_TRUE);
+                Tuple ifFalse = expression.getTupleProperty(Property.KEY_IF_FALSE);
+
+                Label endLabel = generator.newLabel();
+                Label ternaryIfFalseLabel = generator.newLabel();
+
+                String resultType = populateMethodExpression(generator, methodScope, condition, false, ternaryIfFalseLabel, false);
+                String ifTrueResultType = populateMethodExpression(generator, methodScope, ifTrue, false, null, true);
+                generator.goTo(endLabel);
+                generator.visitLabel(ternaryIfFalseLabel);
+                String ifFalseResultType = populateMethodExpression(generator, methodScope, ifFalse, false, null, true);
+                generator.visitLabel(endLabel);
+                break;
             }
         }
 
