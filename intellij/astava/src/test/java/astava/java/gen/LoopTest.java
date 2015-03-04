241ea302e6e19462f15184f7982273ff0756d7c4
jakobehmsen <jakobehmsen.github@gmail.com>
2015-03-04 19:35:02 +0100

    Added test for loop with break statement.

--- intellij/astava/src/test/java/astava/java/gen/LoopTest.java
+++ intellij/astava/src/test/java/astava/java/gen/LoopTest.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
+import java.util.Collections;
 
 import static astava.CommonTest.testMethodBody;
 import static astava.java.Factory.*;
@@ -22,13 +23,38 @@ public class LoopTest {
         Tuple ast = block(Arrays.asList(
             declareVar(Descriptor.INT, "i"),
             assignVar("i", literal(0)),
-            loop(compare(accessVar("i"), literal(repeat), RelationalOperator.LT),
+            loop(lt(accessVar("i"), literal(repeat)),
                 intIncVar("i", IncTiming.POST, 1)
             ),
             ret(accessVar("i"))
         ));
 
         testMethodBody(ast, Descriptor.INT, actualValue ->
-                assertEquals(expectedValue, actualValue));
+            assertEquals(expectedValue, actualValue));
+    }
+
+    @Test
+    public void testLoopWithBreak() throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+        int repeat = 10;
+        Object expectedValue = repeat;
+
+        Tuple b = brk();
+
+        Tuple ast = block(Arrays.asList(
+            declareVar(Descriptor.INT, "i"),
+            assignVar("i", literal(0)),
+            loop(literal(true),
+                block(Arrays.asList(
+                    ifElse(ge(accessVar("i"), literal(repeat)),
+                        brk(),
+                        block(Collections.emptyList())),
+                    intIncVar("i", IncTiming.POST, 1)
+                ))
+            ),
+            ret(accessVar("i"))
+        ));
+
+        testMethodBody(ast, Descriptor.INT, actualValue ->
+            assertEquals(expectedValue, actualValue));
     }
 }
