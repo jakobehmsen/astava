a080e733ef84b40f16098985a9f2f9ef40aa78c6
jakobehmsen <jakobehmsen.github@gmail.com>
2015-03-08 12:43:49 +0100

    Added test for switch statements.

---
+++ intellij/astava/src/test/java/astava/java/gen/SwitchTest.java
@@ -0,0 +1,128 @@
+package astava.java.gen;
+
+import astava.CommonTest;
+import astava.core.Tuple;
+import astava.java.Descriptor;
+import org.junit.Test;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+
+import static astava.java.Factory.*;
+import static org.junit.Assert.assertEquals;
+
+public class SwitchTest {
+    @Test
+    public void testSwitchFirstCase() throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+        Tuple ast = block(Arrays.asList(
+            declareVar(Descriptor.INT, "result"),
+            select(literal(0),
+                Arrays.asList(
+                    option(0, block(Arrays.asList(assignVar("result", literal(0)), brk()))),
+                    option(1, block(Arrays.asList(assignVar("result", literal(1)), brk()))),
+                    option(2, block(Arrays.asList(assignVar("result", literal(2)), brk())))
+                ),
+                assignVar("result", literal(-1))
+            ),
+            ret(accessVar("result"))
+        ));
+
+        CommonTest.testMethodBody(ast, Descriptor.INT, actualValue ->
+            assertEquals(0, actualValue));
+    }
+
+    @Test
+    public void testSwitchSecondCase() throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+        Tuple ast = block(Arrays.asList(
+            declareVar(Descriptor.INT, "result"),
+            select(literal(1),
+                Arrays.asList(
+                    option(0, block(Arrays.asList(assignVar("result", literal(0)), brk()))),
+                    option(1, block(Arrays.asList(assignVar("result", literal(1)), brk()))),
+                    option(2, block(Arrays.asList(assignVar("result", literal(2)), brk())))
+                ),
+                assignVar("result", literal(-1))
+            ),
+            ret(accessVar("result"))
+        ));
+
+        CommonTest.testMethodBody(ast, Descriptor.INT, actualValue ->
+            assertEquals(1, actualValue));
+    }
+
+    @Test
+    public void testSwitchThirdCase() throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+        Tuple ast = block(Arrays.asList(
+            declareVar(Descriptor.INT, "result"),
+            select(literal(2),
+                Arrays.asList(
+                    option(0, block(Arrays.asList(assignVar("result", literal(0)), brk()))),
+                    option(1, block(Arrays.asList(assignVar("result", literal(1)), brk()))),
+                    option(2, block(Arrays.asList(assignVar("result", literal(2)), brk())))
+                ),
+                assignVar("result", literal(-1))
+            ),
+            ret(accessVar("result"))
+        ));
+
+        CommonTest.testMethodBody(ast, Descriptor.INT, actualValue ->
+            assertEquals(2, actualValue));
+    }
+
+    @Test
+    public void testSwitchFirstToThirdCase() throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+        Tuple ast = block(Arrays.asList(
+            declareVar(Descriptor.INT, "result"),
+            select(literal(0),
+                Arrays.asList(
+                    option(0, block(Arrays.asList(assignVar("result", literal(0))))), // Fallthrough
+                    option(1, block(Arrays.asList(assignVar("result", literal(1))))), // Fallthrough
+                    option(2, block(Arrays.asList(assignVar("result", literal(2)), brk())))
+                ),
+                assignVar("result", literal(-1))
+            ),
+            ret(accessVar("result"))
+        ));
+
+        CommonTest.testMethodBody(ast, Descriptor.INT, actualValue ->
+            assertEquals(2, actualValue));
+    }
+
+    @Test
+    public void testSwitchDefaultCaseByGT() throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+        Tuple ast = block(Arrays.asList(
+            declareVar(Descriptor.INT, "result"),
+            select(literal(3),
+                Arrays.asList(
+                    option(0, block(Arrays.asList(assignVar("result", literal(0)), brk()))),
+                    option(1, block(Arrays.asList(assignVar("result", literal(1)), brk()))),
+                    option(2, block(Arrays.asList(assignVar("result", literal(2)), brk())))
+                ),
+                assignVar("result", literal(-1))
+            ),
+            ret(accessVar("result"))
+        ));
+
+        CommonTest.testMethodBody(ast, Descriptor.INT, actualValue ->
+            assertEquals(-1, actualValue));
+    }
+
+    @Test
+    public void testSwitchDefaultCaseByLT() throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+        Tuple ast = block(Arrays.asList(
+            declareVar(Descriptor.INT, "result"),
+            select(literal(-1),
+                Arrays.asList(
+                    option(0, block(Arrays.asList(assignVar("result", literal(0)), brk()))),
+                    option(1, block(Arrays.asList(assignVar("result", literal(1)), brk()))),
+                    option(2, block(Arrays.asList(assignVar("result", literal(2)), brk())))
+                ),
+                assignVar("result", literal(-1))
+            ),
+            ret(accessVar("result"))
+        ));
+
+        CommonTest.testMethodBody(ast, Descriptor.INT, actualValue ->
+            assertEquals(-1, actualValue));
+    }
+}
