9200c54cdb57532d3c10ca2fdb5435f6c4424771
jakobehmsen <jakobehmsen.github@gmail.com>
2015-04-04 10:50:26 +0200

    Fixed issue with not mainting order of relations properly. Added postponing of parse process.

--- intellij/astava/src/main/java/astava/samples/virela/view/RelationSetView.java
+++ intellij/astava/src/main/java/astava/samples/virela/view/RelationSetView.java
@@ -13,60 +13,76 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.util.Hashtable;
 import java.util.Map;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 public class RelationSetView extends JPanel {
     private Hashtable<String, JComponent> relationSet = new Hashtable<>();
+    private JPanel contentView = new JPanel();
+
+    private ScheduledExecutorService parseExecutor = Executors.newScheduledThreadPool(1);
+    private ScheduledFuture<?> parseFuture;
 
     public RelationSetView() {
         setLayout(new BorderLayout());
 
-        JPanel contentView = new JPanel();
         add(contentView, BorderLayout.CENTER);
 
         JTextPane scriptView = new JTextPane();
         scriptView.addKeyListener(new KeyAdapter() {
             @Override
-            public void keyPressed(KeyEvent e) {
-                if(e.isControlDown() && e.getKeyCode() == KeyEvent.VK_ENTER) {
-                    String source = scriptView.getText();
+            public void keyReleased(KeyEvent e) {
+                if (parseFuture != null) {
+                    parseFuture.cancel(false);
+                }
 
-                    relationSet.values().forEach(x -> remove(x));
+                parseFuture = parseExecutor.schedule(() -> {
+                    String source = scriptView.getText();
+                    System.out.println("Source:\n" + source);
 
                     RelationParser parser = new RelationParser();
                     Matcher<Character, Relation> matcher = parser.parseInit(new CharSequenceCursor(source), (p, c) -> new CommonMatcher<>());
 
-                    if(matcher.isMatch()) {
-                        //matcher.production().stream().forEach(x -> print(x));
+                    if (matcher.isMatch()) {
+                        contentView.removeAll();
+                        //relationSet.values().forEach(x -> contentView.remove(x));
+
+                        java.util.List<Relation> newRelations = matcher.production().stream().collect(Collectors.toList());
                         Map<String, Expression> newRelationSet = matcher.production().stream().collect(Collectors.toMap(r -> r.getId(), r -> r.getValue()));
 
-                        newRelationSet.entrySet().stream().forEach(x -> {
-                            if (!relationSet.containsKey(x.getKey())) {
+                        System.out.println("Ids: " + newRelations.stream().map(r -> r.getId()).collect(Collectors.toSet()));
+                        System.out.println("Components count: " + contentView.getComponentCount());
+
+                        newRelations.forEach(r -> {
+                            if (!relationSet.containsKey(r.getId())) {
                                 // Insertion
-                                JTextArea relationView = new JTextArea(x.getKey());
-                                relationSet.put(x.getKey(), relationView);
+                                // View should be dependent on expression:
+                                // E.g. int stream should be numeric up down
+                                JTextArea relationView = new JTextArea(r.getId());
+                                relationSet.put(r.getId(), relationView);
                                 contentView.add(relationView);
-                            } {
+                            }
+                            {
                                 // Update
-                                JComponent relationView = relationSet.get(x.getKey());
+                                JComponent relationView = relationSet.get(r.getId());
                                 contentView.add(relationView);
                             }
                         });
 
                         // Deletions
-                        relationSet.entrySet().stream().filter(x -> !newRelationSet.containsKey(x.getKey())).forEach(x -> {
-                            JComponent relationView = relationSet.get(x.getKey());
-                            contentView.remove(relationView);
-                        });
+                        relationSet.keySet().retainAll(newRelations.stream().map(r -> r.getId()).collect(Collectors.toSet()));
 
                         contentView.revalidate();
                         contentView.repaint();
 
-                        System.out.print("Success");
+                        System.out.println("Success, relation set size = " + newRelationSet.size());
                     } else {
-                        System.out.print("Failure");
+                        System.out.println("Failure");
                     }
-                }
+                }, 150, TimeUnit.MILLISECONDS);
             }
         });
         scriptView.setBorder(BorderFactory.createMatteBorder(1, 0, 0, 0, Color.BLACK));
