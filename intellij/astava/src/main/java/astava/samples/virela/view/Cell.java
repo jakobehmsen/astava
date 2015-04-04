7553f322151e563025ee96c3f9cb5bfa3c69a229
jakobehmsen <jakobehmsen.github@gmail.com>
2015-04-04 12:14:44 +0200

    Did a bit of cleaning up.

--- intellij/astava/src/main/java/astava/samples/virela/view/Cell.java
+++ intellij/astava/src/main/java/astava/samples/virela/view/Cell.java
@@ -1,9 +1,6 @@
 package astava.samples.virela.view;
 
 import javax.swing.*;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.function.Consumer;
 
 public abstract class Cell<T> {
     private JComponent view;
@@ -17,20 +14,4 @@ public abstract class Cell<T> {
     }
 
     public abstract void consume(CellConsumer<T> consumer);
-
-    /*public void beRemoved() {
-
-    }
-
-    public void addObserver(Consumer<T> o) {
-        observers.add(o);
-    }
-
-    public void removeObserver(Consumer<T> o) {
-        observers.remove(o);
-    }
-
-    public void propogate(T value) {
-        observers.forEach(o -> o.accept(value));
-    }*/
 }
