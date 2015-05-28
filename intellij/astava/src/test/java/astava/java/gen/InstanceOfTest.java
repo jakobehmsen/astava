29bffe1e7cd1e2b096a5fa61eb2eaedbe915201b
jakobehmsen <jakobehmsen.github@gmail.com>
2015-05-28 09:59:32 +0200

    Added usage of AST replacement in test InstanceOfTest.

--- intellij/astava/src/test/java/astava/java/gen/InstanceOfTest.java
+++ intellij/astava/src/test/java/astava/java/gen/InstanceOfTest.java
@@ -1,14 +1,15 @@
 package astava.java.gen;
 
-import astava.CommonTest;
+import astava.CommonTestDom;
+import astava.tree.ExpressionDom;
 import astava.tree.Tuple;
 import astava.java.Descriptor;
 import org.junit.Test;
 
 import java.lang.reflect.InvocationTargetException;
 
-import static astava.java.Factory.instanceOf;
-import static astava.java.Factory.literal;
+import static astava.java.FactoryDom.instanceOf;
+import static astava.java.FactoryDom.literal;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
@@ -16,17 +17,17 @@ import static org.junit.Assert.assertTrue;
 public class InstanceOfTest {
     @Test
     public void testInstanceOfTrue() throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException {
-        Tuple ast = instanceOf(literal("str"), Descriptor.STRING);
+        ExpressionDom ast = instanceOf(literal("str"), Descriptor.STRING);
 
-        CommonTest.testExpression(ast, Descriptor.BOOLEAN, actualValue ->
+        CommonTestDom.testExpression(ast, Descriptor.BOOLEAN, actualValue ->
             assertTrue((boolean)actualValue));
     }
 
     @Test
     public void testInstanceOfFalse() throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException {
-        Tuple ast = instanceOf(literal("str"), Descriptor.get(Number.class));
+        ExpressionDom ast = instanceOf(literal("str"), Descriptor.get(Number.class));
 
-        CommonTest.testExpression(ast, Descriptor.BOOLEAN, actualValue ->
+        CommonTestDom.testExpression(ast, Descriptor.BOOLEAN, actualValue ->
             assertFalse((boolean)actualValue));
     }
 }
