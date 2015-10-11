47a1cd941e812f556d21f7c6b0112b203e0c5f9a
jakobehmsen <jakobehmsen.github@gmail.com>
2015-10-11 12:15:28 +0200

    Factored out ClassInspector and ClassResolver logic from sample agent. Cleaned up sample agent.

---
+++ intellij/astava/src/main/java/astava/java/parser/DefaultClassInspector.java
@@ -0,0 +1,149 @@
+package astava.java.parser;
+
+import astava.java.Descriptor;
+import astava.tree.FieldDom;
+import astava.tree.MethodDom;
+import astava.tree.ParameterInfo;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static astava.java.DomFactory.fieldDeclaration;
+
+public class DefaultClassInspector implements ClassInspector {
+    private ClassLoader classLoader;
+
+    public DefaultClassInspector(ClassLoader classLoader) {
+        this.classLoader = classLoader;
+    }
+
+    @Override
+    public ClassDeclaration getClassDeclaration(String name) {
+        // Inspect virtual classes in class builders and physical classes in class loader
+        try {
+            Class<?> physicalClass = classLoader.loadClass(name);
+
+            //logln("Loaded physical class " + name);
+
+            return new ClassDeclaration() {
+                @Override
+                public List<FieldDeclaration> getFields() {
+                    return Arrays.asList(physicalClass.getDeclaredFields()).stream().map(x -> new FieldDeclaration() {
+                        @Override
+                        public int getModifier() {
+                            return x.getModifiers();
+                        }
+
+                        @Override
+                        public String getTypeName() {
+                            return x.getType().getName();
+                        }
+
+                        @Override
+                        public String getName() {
+                            return x.getName();
+                        }
+
+                        @Override
+                        public FieldDom build(ClassDeclaration classDeclaration) {
+                            return fieldDeclaration(getModifier(), getName(), getTypeName());
+                        }
+                    }).collect(Collectors.toList());
+                }
+
+                @Override
+                public List<MethodDeclaration> getMethods() {
+                    List<MethodDeclaration> methods = Arrays.asList(physicalClass.getDeclaredMethods()).stream().map(x -> new MethodDeclaration() {
+                        @Override
+                        public int getModifier() {
+                            return x.getModifiers();
+                        }
+
+                        @Override
+                        public String getName() {
+                            return x.getName();
+                        }
+
+                        @Override
+                        public List<ParameterInfo> getParameterTypes() {
+                            return Arrays.asList(x.getParameterTypes()).stream()
+                                .map(x -> new ParameterInfo(Descriptor.get(x), "<NA>"))
+                                .collect(Collectors.toList());
+                        }
+
+                        @Override
+                        public String getReturnTypeName() {
+                            return x.getReturnType().getName();
+                        }
+
+                        @Override
+                        public MethodDom build(ClassDeclaration classDeclaration, ClassInspector classInspector) {
+                            return null;
+                        }
+                    }).collect(Collectors.toList());
+
+                    List<MethodDeclaration> constructors = Arrays.asList(physicalClass.getConstructors()).stream().map(x -> new MethodDeclaration() {
+                        @Override
+                        public int getModifier() {
+                            return x.getModifiers();
+                        }
+
+                        @Override
+                        public String getName() {
+                            return "<init>";
+                        }
+
+                        @Override
+                        public List<ParameterInfo> getParameterTypes() {
+                            return Arrays.asList(x.getParameterTypes()).stream()
+                                .map(x -> new ParameterInfo(Descriptor.get(x), "<NA>"))
+                                .collect(Collectors.toList());
+                        }
+
+                        @Override
+                        public String getReturnTypeName() {
+                            return "void";
+                        }
+
+                        @Override
+                        public MethodDom build(ClassDeclaration classDeclaration, ClassInspector classInspector) {
+                            return null;
+                        }
+                    }).collect(Collectors.toList());
+
+                    return Stream.concat(methods.stream(), constructors.stream()).collect(Collectors.toList());
+                }
+
+                @Override
+                public int getModifiers() {
+                    return physicalClass.getModifiers();
+                }
+
+                @Override
+                public String getName() {
+                    return physicalClass.getName();
+                }
+
+                @Override
+                public String getSuperName() {
+                    return physicalClass.getSuperclass().getName();
+                }
+
+                @Override
+                public List<String> getInterfaces() {
+                    return Arrays.asList(physicalClass.getInterfaces()).stream().map(x -> x.getName()).collect(Collectors.toList());
+                }
+
+                @Override
+                public boolean isInterface() {
+                    return physicalClass.isInterface();
+                }
+            };
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
---
+++ intellij/astava/src/main/java/astava/java/parser/DefaultClassResolver.java
@@ -0,0 +1,49 @@
+package astava.java.parser;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class DefaultClassResolver implements ClassResolver {
+    private ClassLoader classLoader;
+    private Map<String, String> simpleNameToNameMap;
+    private Set<String> nameSet;
+
+    public DefaultClassResolver(ClassLoader classLoader, List<Class<?>> classes) {
+        this(classLoader, classes.stream()
+            .map(x -> new AbstractMap.SimpleImmutableEntry<>(x.getSimpleName(), x.getName()))
+            .collect(Collectors.toMap(x -> x.getKey(), x -> x.getValue())));
+    }
+
+    public DefaultClassResolver(ClassLoader classLoader, Map<String, String> simpleNameToNameMap) {
+        this.classLoader = classLoader;
+        this.simpleNameToNameMap = simpleNameToNameMap;
+        nameSet = simpleNameToNameMap.values().stream().collect(Collectors.toSet());
+    }
+
+    @Override
+    public boolean canResolveAmbiguous(String className) {
+        if(nameSet.contains(className))
+            return true;
+
+        if(className.endsWith("[]")) {
+            try {
+                Class.forName("[L" + className.substring(0, className.length() - 2) + ";");
+                return true;
+            } catch (ClassNotFoundException e) {
+                return false;
+            }
+        }
+
+        try {
+            classLoader.loadClass(className);
+            return true;
+        } catch (ClassNotFoundException e) {
+            return false;
+        }
+    }
+
+    @Override
+    public String resolveSimpleName(String className) {
+        return simpleNameToNameMap.get(className);
+    }
+}
