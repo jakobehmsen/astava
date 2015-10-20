018a69d531a17b0d2fd017e0d9b4ba78059f9341
jakobehmsen <jakobehmsen.github@gmail.com>
2015-10-20 13:07:18 +0200

    Attempted to implement a more correct ClassLoader.

--- intellij/astava/src/main/java/astava/java/agent/ClassLoaderExtender.java
+++ intellij/astava/src/main/java/astava/java/agent/ClassLoaderExtender.java
@@ -9,14 +9,15 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
-import java.util.Hashtable;
+import java.util.Arrays;
+import java.util.function.Supplier;
+import java.util.stream.StreamSupport;
 
 public class ClassLoaderExtender extends ClassLoader {
     private ClassNodeExtender extender;
-    private Hashtable<String, Class<?>> cache = new Hashtable<>();
 
     public ClassLoaderExtender(ClassNodeExtender extender) {
-        this.extender = extender;
+        this(ClassLoader.getSystemClassLoader(), extender);
     }
 
     public ClassLoaderExtender(ClassLoader parent, ClassNodeExtender extender) {
@@ -25,140 +26,25 @@ public class ClassLoaderExtender extends ClassLoader {
     }
 
     @Override
-    protected Class<?> findClass(String name) throws ClassNotFoundException {
-        System.out.println("Finding class " + name);
+    protected Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException {
+        System.out.println("Loading class " + name);
 
-        if (cache.containsKey(name)) {
-            return cache.get(name);
-        }
-
-        byte[] classData;
-
-        try {
-            classData = loadClassData(name);
-        } catch (IOException e) {
-            throw new ClassNotFoundException("Class [" + name
-                + "] could not be found", e);
-        }
-
-        Class<?> c = defineClass(name, classData, 0, classData.length);
-        resolveClass(c);
-        cache.put(name, c);
-
-        return c;
-    }
-
-    private byte[] loadClassData(String name) throws IOException {
-
-        InputStream resource = null;//getResourceAsStream(name);
-        try {
-            resource = Class.forName(name).getResourceAsStream(name.substring(name.lastIndexOf('.') + 1) + ".class");
-            ClassReader cr = new ClassReader(resource);
-
-            ClassNode classNode = new ClassNode(Opcodes.ASM5);
-            cr.accept(classNode, org.objectweb.asm.ClassReader.EXPAND_FRAMES);
-
-            System.out.println(classNode.name);
-
-            extender.transform(classNode);
-
-            ClassWriter classWriter = new ClassWriter(ClassWriter.COMPUTE_FRAMES | ClassWriter.COMPUTE_MAXS);
-            classNode.accept(classWriter);
-            ByteArrayOutputStream os = new ByteArrayOutputStream();
-
-            PrintWriter ps = new PrintWriter(os);
-            org.objectweb.asm.util.CheckClassAdapter.verify(new org.objectweb.asm.ClassReader(classWriter.toByteArray()), true, ps);
-            //cr.accept(new TraceClassVisitor(new PrintWriter(System.out)), 0);
-
-            return classWriter.toByteArray();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        return null;
-
-        /*try {
-            ClassReader cr = new ClassReader(resource);
-
-            ClassNode classNode = new ClassNode(Opcodes.ASM5);
-            cr.accept(classNode, org.objectweb.asm.ClassReader.EXPAND_FRAMES);
-
-            System.out.println(classNode.name);
-
-            extender.transform(classNode);
-
-            ClassWriter classWriter = new ClassWriter(ClassWriter.COMPUTE_FRAMES | ClassWriter.COMPUTE_MAXS);
-            classNode.accept(classWriter);
-            ByteArrayOutputStream os = new ByteArrayOutputStream();
-
-            PrintWriter ps = new PrintWriter(os);
-            org.objectweb.asm.util.CheckClassAdapter.verify(new org.objectweb.asm.ClassReader(classWriter.toByteArray()), true, ps);
-            //cr.accept(new TraceClassVisitor(new PrintWriter(System.out)), 0);
-
-            return classWriter.toByteArray();
-        } catch (SecurityException e) {
-            return super.findClass(name);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch(NoClassDefFoundError e) {
-            return super.findClass(name);
-        }
-        return super.findClass(name);*/
-    }
-
-    /*@Override
-    protected Class<?> findClass(String name) throws ClassNotFoundException {
-        System.out.println("Finding class " + name);
-
-        Class<?> c = cache.get(name);
+        Class<?> c = findLoadedClass(name);
 
         if(c != null)
             return c;
 
-        InputStream resource = Class.forName(name).getResourceAsStream(name.substring(name.lastIndexOf('.') + 1) + ".class");//getResourceAsStream(name);
+        c = findClass(name);
 
-        try {
-            ClassReader cr = new ClassReader(resource);
-
-            ClassNode classNode = new ClassNode(Opcodes.ASM5);
-            cr.accept(classNode, org.objectweb.asm.ClassReader.EXPAND_FRAMES);
-
-            System.out.println(classNode.name);
-
-            extender.transform(classNode);
-
-            ClassWriter classWriter = new ClassWriter(ClassWriter.COMPUTE_FRAMES | ClassWriter.COMPUTE_MAXS);
-            classNode.accept(classWriter);
-            ByteArrayOutputStream os = new ByteArrayOutputStream();
-
-            PrintWriter ps = new PrintWriter(os);
-            org.objectweb.asm.util.CheckClassAdapter.verify(new org.objectweb.asm.ClassReader(classWriter.toByteArray()), true, ps);
-            //cr.accept(new TraceClassVisitor(new PrintWriter(System.out)), 0);
-
-            byte[] bytes = classWriter.toByteArray();
-            c = defineClass(name, bytes, 0, bytes.length);
-            cache.put(name, c);
+        if(c != null)
             return c;
-        } catch (SecurityException e) {
-            return super.findClass(name);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch(NoClassDefFoundError e) {
-            return super.findClass(name);
-        }
-        return super.findClass(name);
-    }*/
 
-    /*@Override
-    protected Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException {
-        return findClass(name);
-    }*/
+        return getParent() != null ? getParent().loadClass(name) : findSystemClass(name);
+    }
 
     @Override
-    protected Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException {
-        System.out.println("Loading class " + name);
-
-        InputStream resource = Class.forName(name).getResourceAsStream(name.substring(name.lastIndexOf('.') + 1) + ".class");//getResourceAsStream(name);
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+        InputStream resource = Class.forName(name).getResourceAsStream(name.substring(name.lastIndexOf('.') + 1) + ".class");
 
         try {
             ClassReader cr = new ClassReader(resource);
@@ -179,16 +65,14 @@ public class ClassLoaderExtender extends ClassLoader {
             //cr.accept(new TraceClassVisitor(new PrintWriter(System.out)), 0);
 
             byte[] bytes = classWriter.toByteArray();
+
             return defineClass(name, bytes, 0, bytes.length);
-        } catch (SecurityException e) {
-            //throw new ClassNotFoundException(name);
-            //return super.loadClass(name);
-            return getParent().loadClass(name);
         } catch (IOException e) {
             e.printStackTrace();
-        } catch(NoClassDefFoundError e) {
-            return super.loadClass(name);
+        } catch (SecurityException e) {
+
         }
-        return super.loadClass(name);
+
+        return null;
     }
 }
